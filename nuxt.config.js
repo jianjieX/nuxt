863a3adc8ec14eaddb61b043e1fdee46f892d4ef
EDZ <jianjie_x@163.com>
2019-11-27 09:44:17 +0800

    配置sass

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,8 @@ module.exports = {
   ** Global CSS
   */
   css: [
-    'iview/dist/styles/iview.css'
+    'iview/dist/styles/iview.css',
+    '@/assets/css/main.scss'
   ],
   /*
   ** Plugins to load before mounting the App
